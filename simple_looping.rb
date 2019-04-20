c8aa971dce007106f85e3bbe99a5a1f68ce4f821
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-20 12:18:21 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -54,6 +54,7 @@ def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for counter in 1 .. 7 do 
     puts "Welcome to Flatiron School's Web Development Course!"
+    
   
 end
 
